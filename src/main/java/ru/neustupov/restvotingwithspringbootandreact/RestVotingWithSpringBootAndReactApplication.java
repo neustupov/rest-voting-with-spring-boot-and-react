cac18f71119c38bfcdcf6a9e045d635ea4aeffa6
neustupov <neustupov@yandex.ru>
2018-09-25 18:06:29 +0300

    Start with spring boot

---
+++ src/main/java/ru/neustupov/restvotingwithspringbootandreact/RestVotingWithSpringBootAndReactApplication.java
@@ -0,0 +1,12 @@
+package ru.neustupov.restvotingwithspringbootandreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestVotingWithSpringBootAndReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestVotingWithSpringBootAndReactApplication.class, args);
+	}
+}
