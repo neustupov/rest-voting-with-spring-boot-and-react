e31391232eac9b899fb92854238aaf4e3e78d535
neustupov <neustupov@yandex.ru>
2018-11-05 20:36:34 +0300

    deleting unused imports

--- src/main/java/ru/neustupov/restvotingwithspringbootandreact/repository/RestaurantRepository.java
+++ src/main/java/ru/neustupov/restvotingwithspringbootandreact/repository/RestaurantRepository.java
@@ -1,8 +1,6 @@
 package ru.neustupov.restvotingwithspringbootandreact.repository;
 
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import ru.neustupov.restvotingwithspringbootandreact.model.Restaurant;
 
 import java.util.List;
