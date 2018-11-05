2e3af72e8805fe02cd0241a4110bea615d582d65
neustupov <neustupov@yandex.ru>
2018-11-05 20:37:43 +0300

    editing "convertRestaurantToRestaurantTo" method to lambdas

--- src/main/java/ru/neustupov/restvotingwithspringbootandreact/utils/RestaurantUtil.java
+++ src/main/java/ru/neustupov/restvotingwithspringbootandreact/utils/RestaurantUtil.java
@@ -1,14 +1,13 @@
 package ru.neustupov.restvotingwithspringbootandreact.utils;
 
+import lombok.var;
 import ru.neustupov.restvotingwithspringbootandreact.model.Menu;
 import ru.neustupov.restvotingwithspringbootandreact.model.Restaurant;
 import ru.neustupov.restvotingwithspringbootandreact.model.VoteCount;
 import ru.neustupov.restvotingwithspringbootandreact.to.RestaurantWithTodaysMenu;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class RestaurantUtil {
 
@@ -16,38 +15,37 @@ public class RestaurantUtil {
                                                                                  Collection<Menu> menus,
                                                                                  Collection<VoteCount> voteCounts) {
 
-        List<RestaurantWithTodaysMenu> restaurantWithTodaysMenus = new ArrayList<>();
+        return restaurants.stream().map(restaurant -> {
 
-        for (Restaurant restaurant : restaurants) {
-
-            RestaurantWithTodaysMenu restaurantWithMeal = new RestaurantWithTodaysMenu();
+            var restaurantWithMeal = new RestaurantWithTodaysMenu();
 
             restaurantWithMeal.setId(restaurant.getId());
             restaurantWithMeal.setName(restaurant.getName());
 
-            for (VoteCount voteCount : voteCounts) {
-                if (voteCount.getRestId().equals(restaurant.getId())) {
-                    restaurantWithMeal.setNumberOfVotes(voteCount.getVoteCount().intValue());
-                }
-            }
+            Optional<VoteCount> count = voteCounts
+                    .stream()
+                    .filter(voteCount -> voteCount.getRestId().equals(restaurant.getId()))
+                    .findFirst();
 
-            for (Menu menu : menus) {
-                if (restaurant.getId().equals(menu.getRestaurant().getId())) {
-                    restaurantWithMeal.setMealsFromTodaysMenu(new ArrayList<>(menu.getMeals()));
-                }
-            }
-
-            if (restaurantWithMeal.getNumberOfVotes() == null) {
+            if (count.isPresent()) {
+                restaurantWithMeal.setNumberOfVotes(count.get().getVoteCount().intValue());
+            } else {
                 restaurantWithMeal.setNumberOfVotes(0);
             }
 
-            if(restaurantWithMeal.getMealsFromTodaysMenu() == null) {
+            Optional<Menu> menu = menus
+                    .stream()
+                    .filter(menuOne -> menuOne.getRestaurant().getId().equals(restaurant.getId()))
+                    .findFirst();
+
+            if (menu.isPresent()) {
+                restaurantWithMeal.setMealsFromTodaysMenu(new ArrayList<>(menu.get().getMeals()));
+            } else {
                 restaurantWithMeal.setMealsFromTodaysMenu(Collections.emptyList());
             }
 
-            restaurantWithTodaysMenus.add(restaurantWithMeal);
-        }
+            return restaurantWithMeal;
 
-        return restaurantWithTodaysMenus;
+        }).collect(Collectors.toList());
     }
 }
